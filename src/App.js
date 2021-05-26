a8794ca29cad498a97208f705394edae56e1f747
Susan Holland <segh@fastmail.com>
2021-05-25 21:18:50 -0400

    Fetch sanity and shopify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import sanityClient from './client';
+import client from './shopify';
+import Cart from './components/Cart';
 
 function App() {
+  const [products, setProducts] = useState(null);
+  const [checkout, setCheckout] = useState({ lineItems: [] });
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null)
+
+  useEffect(() => {
+    sanityClient.fetch(
+      `*[_type == 'product']{...}`
+    ).then(data => setProducts(data)).catch(err => console.log(err));
+
+    client.checkout.create().then(res => setCheckout(res)).catch(err => console.log(err));
+
+    client.product.fetchAll().then(products => {
+      console.log(products)
+      setSelectedProduct(products[0])
+    }).catch(err => console.log(err));
+
+  }, []);
+
+  const handleCartClose = () => setIsCartOpen(false);
+
+  const updateQuantityInCart = (lineItemId, quantity) => {
+    const checkoutId = checkout.id;
+    const lineItemsToUpdate = [{ id: lineItemId, quantity: parseInt(quantity, 10)}]
+
+    return client.checkout.updateLineItems(checkoutId, lineItemsToUpdate).then(res => setCheckout(res));
+  }
+
+  const removeLineItemInCart = lineItemId => {
+    const checkoutId = checkout.id;
+    
+    return client.checkout.removeLineItems(checkoutId, [lineItemId]).then(res => setCheckout(res));
+  }
+
+  const addVariantToCart = (id, quantity) => {
+    setIsCartOpen(true);
+
+    const lineItemsToAdd = [{variantId: id, quantity: parseInt(quantity, 10)}]
+    const checkoutId = checkout.id
+
+    return client.checkout.addLineItems(checkoutId, lineItemsToAdd).then(res => setCheckout(res)).catch(err => console.log(err));
+  }
+
+  const selectVariant = () => {
+    const selectedVariant = client.product.helpers.variantForOptions(selectedProduct, { Size: "S"});
+    console.log(selectedVariant)
+    return selectedVariant.id
+  }
+
+  console.log(checkout)
+  console.log(products)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {products && products.map(product => <p onClick={() => addVariantToCart(selectVariant(), 1)}>{product.title}</p>)}
+      <Cart checkout={checkout} isCartOpen={isCartOpen} handleCartClose={handleCartClose} updateQuantityInCart={updateQuantityInCart} removeLineItemInCart={removeLineItemInCart} />
     </div>
   );
 }
