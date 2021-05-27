b899d8a8215ed74e7e105c1dd124678031c5f14d
Susan Holland <segh@fastmail.com>
2021-05-27 16:03:11 -0400

    Merge shopify and sanity product data

--- src/App.js
+++ src/App.js
@@ -12,18 +12,31 @@ function App() {
   const [selectedProduct, setSelectedProduct] = useState(null)
 
   useEffect(() => {
-    sanityClient.fetch(
-      `*[_type == 'product']{...}`
-    ).then(data => setProducts(data)).catch(err => console.log(err));
-
     client.checkout.create().then(res => setCheckout(res)).catch(err => console.log(err));
 
-    client.product.fetchAll().then(products => {
-      const enc = new Base64()
-      const productID = enc.decode(products[0].id).split('gid://shopify/Product/')[1]
-      console.log(productID)
-      console.log(products)
-      setSelectedProduct(products[0])
+    client.product.fetchAll().then(shopifyProducts => {
+      sanityClient.fetch(
+        `*[_type == 'product']{...}`
+      ).then(sanityProducts => {
+        // console.log(sanityProducts)
+        const allProducts = []
+        shopifyProducts.forEach(product => {
+          const enc = new Base64()
+          const productID = parseInt(enc.decode(product.id).split('gid://shopify/Product/')[1])
+          const productDetails = sanityProducts.filter(productDetails => productDetails.productID === productID)[0]
+          // console.log(productDetails)
+          const productData = {
+            ...product,
+            ...productDetails
+          }
+          // console.log(productData)
+          allProducts.push(productData);
+        })
+
+        setProducts(allProducts)
+        // setSelectedProduct(products[0])
+      }).catch(err => console.log(err));
+
     }).catch(err => console.log(err));
 
   }, []);
@@ -52,18 +65,18 @@ function App() {
     return client.checkout.addLineItems(checkoutId, lineItemsToAdd).then(res => setCheckout(res)).catch(err => console.log(err));
   }
 
-  const selectVariant = () => {
-    const selectedVariant = client.product.helpers.variantForOptions(selectedProduct, { Size: "S"});
+  const selectVariant = (product) => {
+    const selectedVariant = client.product.helpers.variantForOptions(product, {[product.options[0].name]: product.options[0].values[0]});
     console.log(selectedVariant)
     return selectedVariant.id
   }
 
-  console.log(checkout)
+  // console.log(checkout)
   console.log(products)
 
   return (
     <div className="App">
-      {products && products.map(product => <p onClick={() => addVariantToCart(selectVariant(), 1)}>{product.title}</p>)}
+      {products && products.map(product => <p key={product.id} onClick={() => addVariantToCart(product.variants[0].id, 1)}>{product.title}</p>)}
       <Cart checkout={checkout} isCartOpen={isCartOpen} handleCartClose={handleCartClose} updateQuantityInCart={updateQuantityInCart} removeLineItemInCart={removeLineItemInCart} />
     </div>
   );
